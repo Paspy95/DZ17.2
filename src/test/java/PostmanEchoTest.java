992fee17e0e3b6571d64f9abf642820a97dacc18
Roman Gudov <gudov.p2@mail.ru>
2024-01-25 00:42:23 +0300

    change test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(2024));
+                .body("data", equalTo(data));
     }
 }
