f7bda2c68cb036464eb5fda2a923df10e560d188
Roman Gudov <gudov.p2@mail.ru>
2024-01-24 21:38:45 +0300

    add test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void dataToBeSend() {
+        var data = "2024";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(data)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(data));
+    }
+}
